c297146fbd24e2062d770a4bedd25e4e8697b413
Marquis4 <marquistumey12@gmail.com>
2023-05-24 07:34:45 -0700

    P7

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-say_my_name = __import__('3-say_my_name').say_my_name
-
-say_my_name("John", "Smith")
-say_my_name("Walter", "White")
-say_my_name("Bob")
-try:
-    say_my_name(12, "White")
-except Exception as e:
-    print(e)
+def say_my_name(first_name, last_name=""):
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print("My name is {:s} {:s}".format(first_name, last_name))
