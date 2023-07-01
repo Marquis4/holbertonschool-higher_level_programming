c59cb2cfcb604a9d48764bb2c3186cbce75e9023
Marquis4 <marquistumey12@gmail.com>
2023-07-01 14:06:42 +0000

    more sql

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,7 +1,4 @@
--- Creates a database "hbtn_0d_2" and a user "user_0d_2"
--- The user should only have SELECT privileges
+-- Script that creates the database hbtn_0d_2 and the user user_0d_2
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS user_0d_2@localhost;
-SET PASSWORD FOR user_0d_2@localhost = 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_0d_2 . * TO user_0d_2@localhost;
-FLUSH PRIVILEGES;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
