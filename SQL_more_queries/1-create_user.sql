c693942b35563f3cea4b26a2baea25ed34d65e42
Marquis4 <marquistumey12@gmail.com>
2023-07-01 14:07:54 +0000

    more sql

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,6 +1,3 @@
--- Creates user "user_0d_1"
--- password set to "user_0d_1_pwd"
-CREATE USER IF NOT EXISTS user_0d_1@localhost;
-SET PASSWORD FOR user_0d_1@localhost = 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost;
-FLUSH PRIVILEGES;
+-- Script that creates the MySQL server user user_0d_1
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
